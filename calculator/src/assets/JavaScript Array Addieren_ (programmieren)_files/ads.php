7181404c1f689209afda60772dd3253d7c486792
Peter Lakatos <peter.lakatos@outlook.de>
2023-05-08 15:44:40 +0200

    com

--- calculator/src/assets/JavaScript Array Addieren_ (programmieren)_files/ads.php
+++
@@ -1,9 +0,0 @@
-Adform=window.Adform||{};
-Adform.host="https://track.adform.net";
-Adform.shost="https://s1.adform.net";
-Adform.scriptBase=Adform.shost+"/banners/scripts/";
-Adform.cache="626";
-Adform.ADFBannerData={"URL":"https://track.adform.net/adfserve/?bn=49556874;extVars=783431|1|affiliate%20postview|av;trackpixel=https://aaa.telekom.de/trck/eview/908cbadb724971cc9012ced767276cc8?ext_publisher_id=783431&b=1&no=1&ctid=__ADFCREID__&tg=__ADFBANGROUP1__&ckurl=1;crdrvar=page;crdrurl=https://www.awin1.com/awclick.php?mid=11430&id=783431&gid=361931&linkid=2950933&pref1=76473000078657106014565012317003&gdpr=&gdpr_consent=&p=https%3A%2F%2Fad3.ad-srv.net%2Fc%2Fpq9xs1bt1bk8066%3Ftprd%3Dhttps%3A%2F%2Ftracking.mlsat02.de%2Ftelekom%2Faff%2Fads.php%3Fdlid%3D2055%26c%3D1&tg=__ADFBANGROUP1__","CREFURL":"https%3a%2f%2fad.ad-srv.net%2f","BN":49556874,"READ_CONSENT":true};
-if(!Adform.ADFBannerParams)Adform.ADFBannerParams=[];
-Adform.ADFBannerParams.push(Adform.ADFBannerData);
-typeof Adform.ADFUtilInstance=="undefined"?document.write('<scr'+'ipt type="text/javascript" src="https://s1.adform.net/stoat/626/s1.adform.net/bootstrap.js"></scr'+'ipt>'):new Adform.ADFBanner();
--- calculator/src/assets/JavaScript Array Addieren_ (programmieren)_files/n.js
+++
@@ -1,1 +0,0 @@
-DOMlessLLDcallback_2820901({"nu": 114686, "oh": 609912, "nm": 528084})
